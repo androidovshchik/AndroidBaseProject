2929cba28399ba83467c947b5a42daf28bd95da8
vlad <gimrcpp@gmail.com>
2018-06-16 08:55:35 +0300

    changes

--- app/src/main/java/io/androidovshchik/demoapp/MainActivity.kt
+++ app/src/main/java/io/androidovshchik/demoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.androidovshchik.demoapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import io.androidovshchik.base.BaseAppCompatActivity
@@ -7,13 +8,13 @@ import io.androidovshchik.base.utils.PermissionUtil
 
 class MainActivity : BaseAppCompatActivity() {
 
+    @SuppressLint("LogNotTimber")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Log.d("sa", PermissionsUtil.getPermissions(applicationContext).toString())
         PermissionUtil.request(applicationContext, *PermissionUtil.allPermissions(applicationContext))
             .subscribe {
-                Log.d("it", "RESULT " + it)
+                Log.d("Permissions", "GRANTED $it")
             }
     }
 }
--- base-project/src/main/java/io/androidovshchik/base/utils/NotificationUtil.kt
+++ base-project/src/main/java/io/androidovshchik/base/utils/NotificationUtil.kt
@@ -16,16 +16,12 @@ object NotificationUtil {
     private const val NOISY_CHANNEL_ID = "noisy_channel"
     private const val QUITE_CHANNEL_ID = "quite_channel"
 
-    private fun provideChannels(context: Context) {
+    fun buildSilent(context: Context, title: String, @DrawableRes icon: Int): Notification {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(NotificationChannel(QUITE_CHANNEL_ID,
                 context.getString(R.string.library_name), NotificationManager.IMPORTANCE_LOW))
         }
-    }
-
-    fun buildSilent(context: Context, title: String, @DrawableRes icon: Int): Notification {
-        provideChannels(context)
         return NotificationCompat.Builder(context, QUITE_CHANNEL_ID)
             .setContentTitle(title)
             .setSmallIcon(icon)
