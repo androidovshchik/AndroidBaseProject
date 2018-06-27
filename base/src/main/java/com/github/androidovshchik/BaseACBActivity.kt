42944232c3f311e652aaaa915ab04eb0ac822fb6
vlad <gimrcpp@gmail.com>
2018-06-27 15:24:57 +0300

    added binding service

---
+++ base/src/main/java/com/github/androidovshchik/BaseACBActivity.kt
@@ -0,0 +1,41 @@
+package com.github.androidovshchik
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+
+@Suppress("MemberVisibilityCanBePrivate")
+abstract class BaseACBActivity<S: BaseFBService> : BaseACActivity() {
+
+    abstract val serviceClass: Class<out BaseFBService>
+
+    var service: S? = null
+
+    override fun onStart() {
+        super.onStart()
+        val intent = Intent(applicationContext, serviceClass)
+        startService(intent)
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (service != null) {
+            unbindService(serviceConnection)
+        }
+    }
+
+    private val serviceConnection = object : ServiceConnection {
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            service = null
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+            service = (binder as BaseFBService.BaseBinder).service as S
+        }
+    }
+}
