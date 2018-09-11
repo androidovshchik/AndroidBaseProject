58cd989649736c8e372aa3a7abc841b76dd86e27
vlad <gimrcpp@gmail.com>
2018-09-11 17:59:39 +0300

    changes

--- core/src/main/java/com/github/androidovshchik/core/BaseApplication.kt
+++ core/src/main/java/com/github/androidovshchik/core/BaseApplication.kt
@@ -3,18 +3,16 @@
 package com.github.androidovshchik.core
 
 import android.app.Application
-import android.text.TextUtils
 import com.github.androidovshchik.core.utils.context.isDebug
-import com.github.androidovshchik.core.utils.context.preferences
-import com.github.androidovshchik.core.utils.getAllSorted
 import com.github.androidovshchik.core.utils.initACRA
-import com.github.androidovshchik.core.utils.newLine
 import org.acra.ACRA
 import timber.log.Timber
 
 open class BaseApplication : Application() {
 
     protected open var dialogTheme = R.style.Library_Dialog
+
+    protected open var replyEmail = "vladkalyuzhnyu@gmail.com"
 
     override fun onCreate() {
         super.onCreate()
@@ -23,8 +21,7 @@ open class BaseApplication : Application() {
         }
         if (isDebug()) {
             Timber.plant(Timber.DebugTree())
-            initACRA(dialogTheme)
+            initACRA(dialogTheme, replyEmail)
         }
-        Timber.d(TextUtils.join(newLine(), preferences.getAllSorted()))
     }
 }
